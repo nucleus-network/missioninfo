6f13d02dc7bbfc996c4f8d320673f641be7adcab
secmorris <smorris@nucleuslearningnetwork.org>
2017-05-14 15:38:14 -0500

    Create activitymenu.js

---
+++ activitymenu.js
@@ -0,0 +1,20 @@
+var title = "Sample Activity Menu Title";
+
+var activities = [
+  {
+    title : "Activity 1",
+    url: "#"
+  },
+  {
+    title : "Activity 2",
+    url: "#"
+  },
+  {
+    title : "Activity 3",
+    url: "#"
+  },
+  {
+    title : "Activity 4",
+    url: "#"
+  }
+];
